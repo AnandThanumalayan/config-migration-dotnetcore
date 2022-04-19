ddbdd16fb1a0b36d3c53a192f6fb223bb10b3cf0
Anand Thanumalayan <anandthanumalayan@Anands-MacBook-Pro.local>
2022-04-19 22:34:02 +0530

    working version

--- Controllers/ConfigController.cs
+++ Controllers/ConfigController.cs
@@ -21,4 +21,18 @@ public class ConfigController : ControllerBase
         var connectionString = _config["ConnectionString"];
         return connectionString;
     }
+
+    // Docker commands to test
+    // $docker build -t configimage .
+    // $docker run -d -p 8080:5763 -e ASPNETCORE_ENVIRONMENT='Staging' configimage
+    // Note: the order of the environment variable and image name is important in the above command
+    
+    // Docker commands to troubleshoot
+    // $docker images
+    // $docker ps
+    // $docker exec 
+
+    // Docker commands to clean up
+    // docker kill $(docker ps -q)
+    // docker rm $(docker ps -a -q)
 }
